c82f72a3ceffcaaa72cd6150eb0e96a76dcb7233
zcong1993 <zhangcong1992@gmail.com>
2019-09-09 20:16:15 +0800

    chore(factory): use default value

--- src/factoryGenerator.ts
+++ src/factoryGenerator.ts
@@ -83,7 +83,7 @@ export class FactoryGenerator {
         {
           name: 'initial',
           type: `Partial<dto.${dtoName}>`,
-          hasQuestionToken: true
+          initializer: '{}'
         }
       ],
       returnType: `dto.${dtoName}`,
